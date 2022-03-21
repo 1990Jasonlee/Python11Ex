4879d135aa950cd9519a7a41b5329b923563e752
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-21 11:18:38 -0400

    area

--- shapes.py
+++ shapes.py
@@ -1,4 +1,7 @@
-class Rectangle():
+class Rectangle:
     def __init__(self, length, width):
         self.length = length
         self.width = width
+
+    def area(self):
+        return self.length * self.width
